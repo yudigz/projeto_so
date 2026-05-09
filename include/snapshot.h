679a8c5b4d1b0e3183d6227a0fdf71fd57938ae1
dev <dev@silentium.htb>
2026-05-09 20:42:54 -0300

    snapshot.c snapshot.h

--- include/snapshot.h
+++ include/snapshot.h
@@ -1,17 +1,15 @@
-#ifndef SISTEMA_H
-#define SISTEMA_H
+#ifndef SNAPSHOT_H
+#define SNAPSHOT_H
 
 #include "tcb.h"
 #include "cpu.h"
 
 typedef struct{
-    int relogio_global;
-    int quantum;
-    char algoritmo[16];
+    int relogio;
     Tcb* tarefas;
     int qtd_tarefas;
     Cpu* cpus;
     int qtd_cpus;
-}SistemaSimulado;
+}Snapshot;
 
 #endif
