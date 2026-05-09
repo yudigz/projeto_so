81b57e09d14d90e8635442323fb38fc1a058ff11
dev <dev@silentium.htb>
2026-05-09 20:12:12 -0300

    sysntax error

--- include/tcb.h
+++ include/tcb.h
@@ -14,18 +14,16 @@ typedef struct{
 }Cor;
 
 typedef struct{
-    int id,
-    Cor cor,
-    int ingresso,
-    int duracao,
-    int duracao_restante,
-    int prioridade,
-    Estado estado,
-    int cpu_atual
+    int id;
+    Cor cor;
+    int ingresso;
+    int duracao;
+    int duracao_restante;
+    int prioridade;
+    Estado estado;
+    int cpu_atual;
     //struct ListaEventos* eventos 
 }Tcb;
-
-
 
 const char* estado_para_string(Estado e);
 
