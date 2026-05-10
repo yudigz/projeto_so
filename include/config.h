bedd3b55c6e92d2262d5c097a246322cfb95f57d
yudigz <yudigunzi@gmail.com>
2026-05-09 22:01:15 -0300

    parser outras linhas

--- include/config.h
+++ include/config.h
@@ -5,8 +5,10 @@
 
 static void strip_newline(char* s);
 
-int ler_config(const char* caminho, SistemaSimulado* sistema);
-
 static int separa_linha_sistema(char* linha, SistemaSimulado* sistema);
+
+static int separa_linha_tarefa(char* linha, Tcb* tcb);
+
+int ler_config(const char* caminho, SistemaSimulado* sistema);
 
 #endif
--- src/config.c
+++ src/config.c
@@ -10,6 +10,56 @@ static void strip_newline(char* s) {
     if (len > 1 && s[len-2] == '\r') s[len-2] = '\0';
 }
 
+static int separa_linha_sistema(char* linha, SistemaSimulado* sistema) {
+    char* token = strtok(linha, ";");
+    if (token == NULL) { /* linha mal formada */ return -1; }
+    strncpy(sistema->algoritmo, token, sizeof(sistema->algoritmo) - 1);
+    sistema->algoritmo[sizeof(sistema->algoritmo) - 1] = '\0';  /* garante terminador */
+
+    token = strtok(NULL, ";");
+    if (token == NULL) return -1;
+    sistema->quantum = atoi(token);
+
+    token = strtok(NULL, ";");
+    if (token == NULL) return -1;
+    sistema->qtd_cpus = atoi(token);
+
+    return 0;
+}
+
+static int separa_linha_tarefa(char* linha, Tcb* tcb){
+    char* token = strtok(linha, ";");
+    if (token == NULL) return -1;
+    tcb->id = atoi(token);
+
+    token = strtok(NULL, ";");
+    if (token == NULL) return -1;
+    unsigned long hex = strtol(token, NULL, 16);
+    tcb->cor.r = (hex >> 16) & 0xFF;
+    tcb->cor.g = (hex >> 8)  & 0xFF;
+    tcb->cor.b =  hex        & 0xFF;
+
+    token = strtok(NULL, ";");
+    if (token == NULL) return -1;
+    tcb->ingresso = atoi(token);
+
+    token = strtok(NULL, ";");
+    if (token == NULL) return -1;
+    tcb->duracao = atoi(token);
+    tcb->duracao_restante = tcb->duracao;  /* começa igual, decrementa depois */
+
+    token = strtok(NULL, ";");
+    if (token == NULL) return -1;
+    tcb->prioridade = atoi(token);
+
+    token = strtok(NULL, ";");
+
+    tcb->estado    = NOVA;   /* toda tarefa começa como NOVA */
+    tcb->cpu_atual = -1;     /* -1 = sem CPU atribuída */
+
+    return 0;
+}
+
 int ler_config(const char* caminho, SistemaSimulado* sistema) {
     FILE* arquivo = fopen(caminho, "r");
     if (arquivo == NULL) {
@@ -30,12 +80,9 @@ int ler_config(const char* caminho, SistemaSimulado* sistema) {
         fclose(arquivo);
         return -1;
     }
-    printf("algoritmo='%s' quantum=%d cpus=%d\n",
-       sistema->algoritmo,
-       sistema->quantum,
-       sistema->qtd_cpus);
+    printf("algoritmo='%s' quantum=%d cpus=%d\n", sistema->algoritmo, sistema->quantum, sistema->qtd_cpus);
 
-    /* Linhas seguintes: uma tarefa por linha */
+     /* Linhas seguintes: uma tarefa por linha */
     while (fgets(linha, sizeof(linha), arquivo) != NULL) {
         strip_newline(linha);
 
@@ -45,23 +92,28 @@ int ler_config(const char* caminho, SistemaSimulado* sistema) {
         printf("Linha tarefa: %s\n", linha);
     }
 
+    sistema->tarefas = NULL;
+    sistema->qtd_tarefas = 0;
+
+    while (fgets(linha, sizeof(linha), arquivo) != NULL) {
+        strip_newline(linha);
+        if (strlen(linha) == 0) continue;
+
+        /* cresce o array em 1 */
+        sistema->tarefas = realloc(sistema->tarefas,
+                                (sistema->qtd_tarefas + 1) * sizeof(Tcb));
+
+        separa_linha_tarefa(linha, &sistema->tarefas[sistema->qtd_tarefas]);
+        sistema->qtd_tarefas++;
+    }
+
+    for (int i = 0; i < sistema->qtd_tarefas; i++) {
+    Tcb* t = &sistema->tarefas[i];
+    printf("Tarefa %d | cor=(%d,%d,%d) | ingresso=%d | duracao=%d | prio=%d\n",
+           t->id, t->cor.r, t->cor.g, t->cor.b,
+           t->ingresso, t->duracao, t->prioridade);
+    }
+
     fclose(arquivo);
-    return 0;
-}
-
-static int separa_linha_sistema(char* linha, SistemaSimulado* sistema) {
-    char* token = strtok(linha, ";");
-    if (token == NULL) { /* linha mal formada */ return -1; }
-    strncpy(sistema->algoritmo, token, sizeof(sistema->algoritmo) - 1);
-    sistema->algoritmo[sizeof(sistema->algoritmo) - 1] = '\0';  /* garante terminador */
-
-    token = strtok(NULL, ";");
-    if (token == NULL) return -1;
-    sistema->quantum = atoi(token);
-
-    token = strtok(NULL, ";");
-    if (token == NULL) return -1;
-    sistema->qtd_cpus = atoi(token);
-
     return 0;
 }
