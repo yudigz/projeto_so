7dcc0a2ac8a08fcc40a3d5e9cd4a8ed4386ffbab
Krause16 <nicholaskrause@alunos.utfpr.edu.br>
2026-05-11 15:54:20 -0300

    priop correct

--- include/escalonador.h
+++ include/escalonador.h
@@ -6,5 +6,6 @@
 Escalonador get_escalonador(const char* nome);
 
 int srtf(SistemaSimulado* sistema, int cpu_id);
+int priop(SistemaSimulado* sistema, int cpu_id);
 
 #endif
--- src/escalonador.c
+++ src/escalonador.c
@@ -21,3 +21,24 @@ int srtf(SistemaSimulado* sistema, int cpu_id) {
 
     return melhor_idx;
 }
+
+int priop(SistemaSimulado* sistema, int cpu_id){
+    int melhor_idx = -1;
+    int melhor_dur = -1;
+
+    for (int i = 0; i < sistema->qtd_tarefas; i++) {
+        Estado e = sistema->tarefas[i].estado;
+        int eh_candidata = (e == PRONTA) ||
+                           (e == EXECUTANDO && sistema->tarefas[i].cpu_atual == cpu_id);
+        if (!eh_candidata) continue;
+
+        int dur = sistema->tarefas[i].prioridade;
+
+        if (melhor_idx == -1 || prio > melhor_prio) {
+            melhor_idx = i;
+            melhor_prio = prio;
+        }
+    }
+
+    return melhor_idx;
+}
